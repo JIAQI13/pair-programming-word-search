5c32414d6f2983ed1599bede34172ec5c3ce2c8d
JIAQI13 <jiaqi11@ualberta.ca>
2021-05-06 03:28:46 +0000

    strech not done

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
+const transpose = function(matrix) {
+    let result = [];
+    let resultArray = [];
+    if (matrix.length === 0) {
+      return [];
+    }
+    //Felicia
+    for (let index = 0; index < matrix[0].length; index++) {
+      for (const layerOne of matrix) {
+        resultArray.push(layerOne[index]);
+      }
+      result.push(resultArray);
+      resultArray = [];
+    }
+    return result;
+  };
+
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
+    const verticalJoin = transpose(letters).map(ls => ls.join(''))
+    //onsole.log(verticalJoin);
+    //console.log(horizontalJoin);
+    for (const l of horizontalJoin) {
         if (l.includes(word)) return true
     }
-}
+    for (const l of verticalJoin) {
+        if (l.includes(word)) return true
+    }
+    return false;
+  };
 
-module.exports = wordSearch
+module.exports = wordSearch;
